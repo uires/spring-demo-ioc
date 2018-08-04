e8fe640bdeb0eae6b38c7f1f743bf61429740683
uires <uiresdeivide1@gmail.com>
2018-08-04 01:17:53 -0300

    carregando o arquivo xml e aplicando ioc

---
+++ src/br/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,14 @@
+package br.com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		Coach theCoach = classPathXmlApplicationContext.getBean("myCoach", Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		classPathXmlApplicationContext.close();
+	}
+}
