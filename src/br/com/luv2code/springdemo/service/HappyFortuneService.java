10e8c4f2b804d89e992d301e28c01aa5fc089607
uires <uiresdeivide1@gmail.com>
2018-08-04 17:30:03 -0300

    adicionando método no happyFortuneService

--- src/br/com/luv2code/springdemo/service/HappyFortuneService.java
+++ src/br/com/luv2code/springdemo/service/HappyFortuneService.java
@@ -2,11 +2,20 @@ package br.com.luv2code.springdemo.service;
 
 import br.com.luv2code.springdemo.IF.FortuneService;
 
-public class HappyFortuneService implements FortuneService{
+public class HappyFortuneService implements FortuneService {
 
 	@Override
 	public String getDailyFortune() {
 		return "Today is your lucky day!";
+	}
+
+	@Override
+	public double[] getFortune() {
+		double[] array = new double[3];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (Math.random() * 1000);
+		}
+		return array;
 	}
 
 }
