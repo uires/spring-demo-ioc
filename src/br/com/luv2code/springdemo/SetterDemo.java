23c8f2e511814373642b67e2b97dcfbe521c284b
uires <uiresdeivide1@gmail.com>
2018-08-04 16:05:14 -0300

    classe main para teste

---
+++ src/br/com/luv2code/springdemo/SetterDemo.java
@@ -0,0 +1,20 @@
+package br.com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import br.com.luv2code.springdemo.entity.CricketCoach;
+
+public class SetterDemo {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		context.close();
+	}
+
+}
