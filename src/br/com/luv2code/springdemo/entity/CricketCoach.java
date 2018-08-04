ebda6e7a6d7d1b479b1d5bcd39ce87f1655c3e2f
uires <uiresdeivide1@gmail.com>
2018-08-04 16:09:24 -0300

    criando getter e setter dos injecting literal values

--- src/br/com/luv2code/springdemo/entity/CricketCoach.java
+++ src/br/com/luv2code/springdemo/entity/CricketCoach.java
@@ -6,18 +6,44 @@ import br.com.luv2code.springdemo.IF.FortuneService;
 public class CricketCoach implements Coach {
 	private FortuneService fortuneService;
 
+	private String emailAddress;
+	private String team;
+
+	public CricketCoach() {
+		System.out.println("Inside no-arg construct");
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method -> setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method -> setTeam");
+		this.team = team;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice 5 stump pentagon Drill";
 	}
 
 	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Inside setter method fortuneService, by spring");
 		this.fortuneService = fortuneService;
 	}
 
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return "Cricket Coach ->" + this.fortuneService.getDailyFortune();
 	}
 
 }
