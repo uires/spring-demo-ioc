03877a47c45274abfb7be3fe00ea0f7e14e483de
uires <uiresdeivide1@gmail.com>
2018-08-04 14:22:23 -0300

    Recebendo fiel para dependecy injection e criando construtor para receber a injeção

--- src/br/com/luv2code/springdemo/BaseballCoach.java
+++ src/br/com/luv2code/springdemo/BaseballCoach.java
@@ -1,11 +1,21 @@
 package br.com.luv2code.springdemo;
 
-public class BaseballCoach implements Coach{
+public class BaseballCoach implements Coach {
+	private FortuneService fortuneService;
+
+	public BaseballCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
