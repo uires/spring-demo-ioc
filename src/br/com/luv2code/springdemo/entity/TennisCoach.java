4b01740875ea7a551f2bbac81b8073d834f1cf77
uires <uiresdeivide1@gmail.com>
2018-08-06 13:37:41 -0300

    nova entidade

---
+++ src/br/com/luv2code/springdemo/entity/TennisCoach.java
@@ -0,0 +1,20 @@
+package br.com.luv2code.springdemo.entity;
+
+import org.springframework.stereotype.Component;
+
+import br.com.luv2code.springdemo.IF.Coach;
+
+@Component("thatTennisCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Pratice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return null;
+	}
+
+}
