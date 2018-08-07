b1d6851120252078ba00fae21c272514f70bf15a
uires <uiresdeivide1@gmail.com>
2018-08-07 12:35:40 -0300

    new enity to pratice @component default bean

--- src/br/com/luv2code/springdemo/BeanDefaultDemoApp.java
+++ src/br/com/luv2code/springdemo/BeanDefaultDemoApp.java
@@ -7,7 +7,7 @@ import br.com.luv2code.springdemo.IF.Coach;
 public class BeanDefaultDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach theCoach = context.getBean("footballCoach", Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		context.close();
 	}
---
+++ src/br/com/luv2code/springdemo/entity/FootballCoach.java
@@ -0,0 +1,20 @@
+package br.com.luv2code.springdemo.entity;
+
+import org.springframework.stereotype.Component;
+
+import br.com.luv2code.springdemo.IF.Coach;
+
+@Component
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice kick to the goal";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Today is your bad day!";
+	}
+
+}
