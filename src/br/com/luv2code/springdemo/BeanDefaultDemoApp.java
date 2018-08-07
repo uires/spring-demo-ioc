89da920c221cba48f67acf1c214842d944213dea
uires <uiresdeivide1@gmail.com>
2018-08-07 12:28:22 -0300

    default bean class test

---
+++ src/br/com/luv2code/springdemo/BeanDefaultDemoApp.java
@@ -0,0 +1,14 @@
+package br.com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import br.com.luv2code.springdemo.IF.Coach;
+
+public class BeanDefaultDemoApp {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		context.close();
+	}
+}
