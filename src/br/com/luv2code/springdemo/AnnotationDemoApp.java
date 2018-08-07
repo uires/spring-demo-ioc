5961345bc054f7591ad746af5ed083481ed1bf23
uires <uiresdeivide1@gmail.com>
2018-08-07 14:05:15 -0300

    @autowired, injeção via anotação e construtor

--- src/br/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/br/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -7,8 +7,9 @@ import br.com.luv2code.springdemo.IF.Coach;
 public class AnnotationDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach theCoach = context.getBean("thatTennisCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 		context.close();
 
 	}
--- src/br/com/luv2code/springdemo/entity/TennisCoach.java
+++ src/br/com/luv2code/springdemo/entity/TennisCoach.java
@@ -1,11 +1,19 @@
 package br.com.luv2code.springdemo.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.luv2code.springdemo.IF.Coach;
+import br.com.luv2code.springdemo.IF.FortuneService;
 
 @Component
 public class TennisCoach implements Coach {
+	private FortuneService fortuneService;
+
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -14,7 +22,7 @@ public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return this.fortuneService.getDailyFortune();
 	}
 
 }
