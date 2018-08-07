d926f3a3111b885be8ae46a6d00223bf9a4ecdf5
uires <uiresdeivide1@gmail.com>
2018-08-07 16:49:37 -0300

    testando scopo

--- src/br/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
+++ src/br/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -1,7 +1,20 @@
 package br.com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import br.com.luv2code.springdemo.IF.Coach;
+
 public class AnnotationBeanScopeDemoApp {
 	public static void main(String[] args) {
-		
+		ClassPathXmlApplicationContext pathXmlApplicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		Coach theCoach = pathXmlApplicationContext.getBean("baseballCoach", Coach.class);
+		Coach alphaCoach = pathXmlApplicationContext.getBean("baseballCoach", Coach.class);
+
+		System.out.println("they are equals: " + (theCoach == alphaCoach));
+		System.out.println("theCoach: " + theCoach);
+		System.out.println("alphaCoach: " + alphaCoach);
+		pathXmlApplicationContext.close();
 	}
 }
