f6283a83311336e12c78cfdd43bc06abece8ed38
uires <uiresdeivide1@gmail.com>
2018-08-07 23:39:07 -0300

    testando configuração

--- src/br/com/luv2code/springdemo/JavaConfigurationDemoApp.java
+++ src/br/com/luv2code/springdemo/JavaConfigurationDemoApp.java
@@ -1,5 +1,18 @@
 package br.com.luv2code.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.com.luv2code.springdemo.IF.Coach;
+import br.com.luv2code.springdemo.configuration.Configuracao;
+
 public class JavaConfigurationDemoApp {
-	
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(
+				Configuracao.class);
+		Coach theCoach = configApplicationContext.getBean("baseballCoach", Coach.class);
+		System.out.println("Testando classe de configuração ->");
+		System.out.println(theCoach.getDailyWorkout());
+		configApplicationContext.close();
+	}
+
 }
