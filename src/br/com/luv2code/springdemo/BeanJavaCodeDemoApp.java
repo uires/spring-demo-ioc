0354525a1d749016281484fef0c1ddcb3193e0a1
uires <uiresdeivide1@gmail.com>
2018-08-08 12:21:26 -0300

    testando bean

--- src/br/com/luv2code/springdemo/BeanJavaCodeDemoApp.java
+++ src/br/com/luv2code/springdemo/BeanJavaCodeDemoApp.java
@@ -1,7 +1,18 @@
 package br.com.luv2code.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.com.luv2code.springdemo.IF.Coach;
+import br.com.luv2code.springdemo.configuration.ConfigurationJavaCodeBeans;
+
 public class BeanJavaCodeDemoApp {
 	public static void main(String[] args) {
-		
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				ConfigurationJavaCodeBeans.class);
+		Coach theCoach = applicationContext.getBean("gymCoach", Coach.class);
+		System.out.println(theCoach.getFortune()[0]);
+		System.out.println(theCoach.getFortune()[1]);
+		System.out.println(theCoach.getFortune()[2]);
+		applicationContext.close();
 	}
 }
